97617b5cd92e20efd085d1e988db22eb62aec333
Kelleymb <kelleybuccieri@gmail.com>
2020-06-30 22:32:39 -0400

    the final commit

--- src/notes-helpers.js
+++ src/notes-helpers.js
@@ -3,7 +3,7 @@ export const findFolder = (folders=[], folder_id) =>
   folders.find(folder => folder.id === folder_id)
 
 export const findNote = (notes=[], noteId) =>
-  notes.find(note => note.id === noteId)
+  notes.find(note => note.id === Number(noteId))
 
 export const getNotesForFolder = (notes=[], folder_id) => (
   (!folder_id)
